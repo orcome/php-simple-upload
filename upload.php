a440fdb0ffc923c35cca12e8deb089db7350dd5f
Akhmad Herdian <akhmad.herdian@gmail.com>
2021-09-23 09:48:33 +0800

    Fixing upload and delete excel file

--- upload.php
+++ upload.php
@@ -1,1 +1,25 @@
 <h3> Upload Dokumen </h3>
+
+<form action="" method="POST" enctype="multipart/form-data">
+    <b> File Upload </b>
+    <input type="file" name="file_upload_name" accept=".xlsx">
+    <input type="submit" name="process" value="Upload">
+</form>
+
+<?php
+
+    if (isset($_POST['process'])) {
+        $directory = "temp/";
+        $fileName = $_FILES['file_upload_name']['name'];
+        $files = glob('temp/*');
+        foreach ($files as $file) {
+            if (is_file($file)) {
+                unlink($file);
+            }
+        }
+        move_uploaded_file($_FILES['file_upload_name']['tmp_name'], $directory.$fileName);
+
+        echo "<b>File berhasil diupload";
+    }
+
+?>
